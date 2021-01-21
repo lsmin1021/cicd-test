2d0d0e5fd06646fcbc265ea05d281cca9bfcf8ac
lsmin1021 <39587551+lsmin1021@users.noreply.github.com>
2021-01-21 16:03:49 +0900

    Update HomeController.java
    배포4

--- src/main/java/com/gradle/controller/HomeController.java
+++ src/main/java/com/gradle/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String index(){
-        return "배포3";
+        return "배포4";
     }
 }
