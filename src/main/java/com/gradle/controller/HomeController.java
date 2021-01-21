5a8c10e7107fe9c579384f6d82d8a237ee8de774
lsmin1021 <dltmdals826@naver.com>
2021-01-21 14:35:53 +0900

    test

--- src/main/java/com/gradle/controller/HomeController.java
+++ src/main/java/com/gradle/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String index(){
-        return "배포완료!";
+        return "배포1";
     }
 }
