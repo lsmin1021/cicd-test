8433d8061625363e49de70d7f70d09fd44e22d79
lsmin1021 <39587551+lsmin1021@users.noreply.github.com>
2021-01-25 15:37:28 +0900

    Update HomeController.java
    slack 연결 확인

--- src/main/java/com/gradle/controller/HomeController.java
+++ src/main/java/com/gradle/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String index(){
-        return "실행 확인";
+        return "slack trigger test";
     }
 }
