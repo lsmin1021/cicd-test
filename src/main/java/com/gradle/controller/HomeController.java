f9421669338eab8ffadc1f04738ae44e227a92b7
lsmin1021 <39587551+lsmin1021@users.noreply.github.com>
2021-01-25 16:45:48 +0900

    Update HomeController.java

--- src/main/java/com/gradle/controller/HomeController.java
+++ src/main/java/com/gradle/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String index(){
-        return "slack trigger test2";
+        return "ci slack test";
     }
 }
