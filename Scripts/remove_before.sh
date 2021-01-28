3b536ce6865ad1b558b1e9ad4ee9e4899550ff31
lsmin1021 <39587551+lsmin1021@users.noreply.github.com>
2021-01-28 14:09:29 +0900

    Update remove_before.sh
    배포 dir 변경

--- Scripts/remove_before.sh
+++ Scripts/remove_before.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-find /opt/codedeploy-agent/deployment-root/641c4afb-530e-451f-b763-325b53f86dbf/* -maxdepth 0 -type 'd' | grep -v $(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/641c4afb-530e-451f-b763-325b53f86dbf /* | sort -t: -n | tail -1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
+find /opt/codedeploy-agent/deployment-root/84e55d14-168e-46d5-abd7-a68840200e69/* -maxdepth 0 -type 'd' | grep -v $(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/84e55d14-168e-46d5-abd7-a68840200e69 /* | sort -t: -n | tail -1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
 sudo rm -rf /home/ec2-user/*
