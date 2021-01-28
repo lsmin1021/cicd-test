cb3c04933de2fe46cbfd3429ebba8e740262ccde
lsmin1021 <39587551+lsmin1021@users.noreply.github.com>
2021-01-28 17:16:29 +0900

    Update HomeController.java

--- src/main/java/com/gradle/controller/HomeController.java
+++ src/main/java/com/gradle/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String index(){
-        return "pipeline 완료";
+        return "재설정";
     }
 }
